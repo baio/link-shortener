1035932015c825fc84727ec1bc70ba74ab218903
Maksim Putilov (Akvelon) <v-maputi@microsoft.com>
2016-07-03 13:45:21 +0300

    debounce-decorator

--- src/app/utils/debounce-decorator.ts
+++ src/app/utils/debounce-decorator.ts
@@ -1,6 +1,28 @@
+import { Subject } from 'rxjs/Subject';
+
 export function debounce(dueTime: number) {
 
-    return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+    return function (target: any, propertyKey: string, descriptor: TypedPropertyDescriptor<Function>) {
+
         console.log(target, propertyKey, descriptor);
+
+        let method = descriptor.value;
+        let targetDestroy = target.ngOnDestroy;
+
+        let subj = new Subject();
+        let debounced = subj.debounceTime(dueTime).distinctUntilChanged();
+
+        let sub = debounced.subscribe((val: any) => method.apply(val.that, val.args));
+
+        target.ngOnDestroy = function () {
+            sub.unsubscribe();
+            if (targetDestroy) {
+                targetDestroy();
+            }
+        }
+
+        descriptor.value = function() {
+            subj.next({that: this, args: arguments});
+        }
     };
 }
